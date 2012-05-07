a68f518f4beaef6195ccb7a6a86ad3e1c45834ab
Sovak <Sovak007@gmail.com>
2012-05-07 16:51:47 +0200

    Riwrited Parser
    Signed-off-by: Sovak <Sovak007@gmail.com>

--- WowheadRipper/Scr/Defines.cs
+++ WowheadRipper/Scr/Defines.cs
@@ -6,77 +6,107 @@ namespace WowheadRipper
 {
     public class Defines
     {
-        public static string fileName = "";
-        public static int programExit = 0;
-        public static Queue<string> stream = new Queue<string>();
-        private static readonly string[] wowhead_raw_name = new string[] {"zone", "object", "object", "object", "item", "item", "item", "npc", "npc", "npc"
-        , "npc"};
-        public static readonly string[] id_name = new string[] { "fishing", "contains", "mining", "herbalism", "contains", "milling", "prospecting","drop"
-        , "skinning", "pickocketing", "engineering"};
-        public static readonly string[] db_name = new string[] { "fishing_loot_temlate", "gameobject_loot_template", "gameobject_loot_template", "gameobject_loot_template"
-        , "item_loot_template", "milling_loot_template", "prospecting_loot_template", "creature_loot_template", "skinning_loot_tem", "pickpocketing_loot_template"
-        , "engineering_loot_template"};
-        public static readonly int maxType = db_name.Length - 1;
-        public static string GenerateWowheadUrl(UInt32 type, UInt32 entry) { return string.Format("http://www.wowhead.com/{0}={1}", wowhead_raw_name[type], entry); }
-        public static Regex GetDataRegex(UInt32 type)
+        private const int maxTypeId = 4;
+        private const int maxSubTypeId = 4;
+        private uint[] maxSubClassTypeId = new uint[maxTypeId];
+        private string[] wowhead_raw_name = new string[maxTypeId];
+        private string[,] id_name = new string[maxTypeId, maxSubTypeId];
+        private string[,] id_db_name = new string[maxTypeId, maxSubTypeId];
+        private Regex[,] id_regex = new Regex[maxTypeId, maxSubTypeId];
+        private Regex[,] id_count_regex = new Regex[maxTypeId, maxSubTypeId];
+        public Queue<string> stream = new Queue<string>();
+        public string fileName = "";
+        public int programExit = 0;
+
+        public uint GetMaxTypeId() { return maxTypeId; }
+        public uint GetMaxSubTypeId(int i) { return maxSubClassTypeId[i]; }
+        public int GetValidFlags(UInt32 TypeId, UInt32 Flags)
         {
-            switch (type)
-            {
-                case 0:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'fishing'.*data: (\[.+\])\}\);");
-                case 1:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*data: (\[.+\])\}\);");
-                case 2:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'mining'.*data: (\[.+\])\}\);");
-                case 3:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'herbalism'.*data: (\[.+\])\}\);");
-                case 4:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*data: (\[.+\])\}\);");
-                case 5:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'milling'.*data: (\[.+\])\}\);");
-                case 6:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'prospecting'.*data: (\[.+\])\}\);");
-                case 7:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'drops'.*data: (\[.+\])\}\);");
-                case 8:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'skinning'.*data: (\[.+\])\}\);");
-                case 9:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'pickpocketing'.*data: (\[.+\])\}\);");
-                case 10:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'engineering'.*data: (\[.+\])\}\);");
-                default:
-                    return new Regex(@"");
-            }
+            int f = 0;
+            for (Int32 i = 0; i < 32; i++)
+                if ((Flags & (1 << i)) == 1)
+                    if (i <= maxSubClassTypeId[i])
+                        f++;
+            return f;
         }
-        public static Regex GetTotalCountRegex(UInt32 type)
+        public string GetOutputName(UInt32 TypeId, UInt32 subTypeId) { return id_name[TypeId, subTypeId]; }
+        public string GetDBName(UInt32 TypeId, UInt32 subTypeId) { return id_db_name[TypeId, subTypeId]; }
+        public string GenerateWowheadUrl(UInt32 typeId, UInt32 entry) { return string.Format("http://www.wowhead.com/{0}={1}", wowhead_raw_name[typeId], entry); }
+        public Regex GetDataRegex(UInt32 TypeId, UInt32 subTypeId) { return id_regex[TypeId, subTypeId]; }
+        public Regex GetTotalCountRegex(UInt32 TypeId, UInt32 subTypeId) { return id_count_regex[TypeId, subTypeId]; }
+
+        public Defines()
         {
-            switch (type)
-            {
-                case 0:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'fishing'.*_totalCount:");
-                case 1:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*computeDataFunc:");
-                case 2:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'mining'.*_totalCount:");
-                case 3:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'herbalism'.*_totalCount:");
-                case 4:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*_totalCount:");
-                case 5:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'milling'.*_totalCount:");
-                case 6:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'prospecting'.*_totalCount:");
-                case 7:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'drops'.*_totalCount:");
-                case 8:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'skinning'.*_totalCount:");
-                case 9:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'pickpocketing'.*_totalCount:");
-                case 10:
-                    return new Regex(@"new Listview\(\{template: 'item', id: 'engineering'.*_totalCount:");
-                default:
-                    return new Regex(@"");
-            }
+            // Zone Parser
+            wowhead_raw_name[0] = "zone";
+            maxSubClassTypeId[0] = 1;
+              // Fishing
+              id_name[0, 0] = "fishing";
+                id_db_name[0, 0] = "fishing_loot_temlate";
+                id_regex[0, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'fishing'.*data: (\[.+\])\}\);");
+                id_count_regex[0, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'fishing'.*_totalCount:");
+
+            // Gameobject Parser
+            wowhead_raw_name[1] = "object";
+            maxSubClassTypeId[1] = 3;
+              // Contains
+              id_name[1, 0] = "contains";
+                id_db_name[1, 0] = "gameobject_loot_template";
+                id_regex[1, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*data: (\[.+\])\}\);");
+                id_count_regex[1, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*computeDataFunc:");
+              // Mining
+              id_name[1, 1] = "mining";
+                id_db_name[1, 1] = "gameobject_loot_template";
+                id_regex[1, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'mining'.*data: (\[.+\])\}\);");
+                id_count_regex[1, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'mining'.*_totalCount:");
+              // Herbalism
+              id_name[1, 2] = "herbalism";
+                id_db_name[1, 2] = "gameobject_loot_template";
+                id_regex[1, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'herbalism'.*data: (\[.+\])\}\);");
+                id_count_regex[1, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'herbalism'.*_totalCount:");
+
+            // Item Parser
+            wowhead_raw_name[2] = "item";
+            maxSubClassTypeId[2] = 3; 
+              // Contains
+              id_name[2, 0] = "contains";
+                id_db_name[2, 0] = "item_loot_template";
+                id_regex[2, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*data: (\[.+\])\}\);");
+                id_count_regex[2, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'contains'.*_totalCount:");
+              // Milling
+              id_name[2, 1] = "milling";
+                id_db_name[2, 1] = "milling_loot_template";
+                id_regex[2, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'milling'.*data: (\[.+\])\}\);");
+                id_count_regex[2, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'milling'.*_totalCount:");
+              // Prospecting
+              id_name[2, 2] = "prospecting";
+                id_db_name[2, 2] = "prospecting_loot_template";
+                id_regex[2, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'prospecting'.*data: (\[.+\])\}\);");
+                id_count_regex[2, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'prospecting'.*_totalCount:");
+
+            // Creature Paser
+            wowhead_raw_name[3] = "npc";
+            maxSubClassTypeId[3] = 4;
+              // Drop
+              id_name[3, 0] = "drop";
+                id_db_name[3, 0] = "creature_loot_template";
+                id_regex[3, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'drops'.*data: (\[.+\])\}\);");
+                id_count_regex[3, 0] = new Regex(@"new Listview\(\{template: 'item', id: 'drops'.*_totalCount:");
+              // Skinning
+              id_name[3, 1] = "skinning";
+                id_db_name[3, 1] = "skinning_loot_tem";
+                id_regex[3, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'skinning'.*data: (\[.+\])\}\);");
+                id_count_regex[3, 1] = new Regex(@"new Listview\(\{template: 'item', id: 'skinning'.*_totalCount:");
+              // Pickpocketing
+              id_name[3, 2] = "pickpocketing";
+                id_db_name[3, 2] = "pickpocketing_loot_template";
+                id_regex[3, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'pickpocketing'.*data: (\[.+\])\}\);");
+                id_count_regex[3, 2] = new Regex(@"new Listview\(\{template: 'item', id: 'pickpocketing'.*_totalCount:");
+              // Engineering
+              id_name[3, 3] = "engineering";
+                id_db_name[3, 3] = "engineering_loot_template";
+                id_regex[3, 3] = new Regex(@"new Listview\(\{template: 'item', id: 'engineering'.*data: (\[.+\])\}\);");
+                id_count_regex[3, 3] = new Regex(@"new Listview\(\{template: 'item', id: 'engineering'.*_totalCount:");
         }
     }
 }
--- WowheadRipper/Scr/Program.cs
+++ WowheadRipper/Scr/Program.cs
@@ -16,12 +16,28 @@ namespace WowheadRipper
 {
     class Program
     {
-        static public UInt32 datad;
-        static public UInt32 datat;
+        static private Defines Def = new Defines();
+        static private UInt32 datad;
+        static private UInt32 datat;
 
         static void AddToStream(string str)
         {
-            Defines.stream.Enqueue(str);
+            Def.stream.Enqueue(str);
+        }
+
+        static void PressExit()
+        {
+            Console.WriteLine("Press any key to continue...");
+        }
+
+        static List<int> ExtractFlags(UInt32 typeId, Int32 num)
+        {
+            List<int> f = new List<int>();
+            for (Int32 i = 0; i < 32; i++)
+                if ((num & (1 << i)) == 1)
+                    if (i <= Def.GetMaxSubTypeId(i))
+                        f.Add(i);
+            return f;
         }
 
         static void Main(string[] args)
@@ -33,25 +49,26 @@ namespace WowheadRipper
             if (args.Length != 2 || !args[0].Contains("-file"))
             {
                 Console.WriteLine("Usage WowheadRipper -file <filename>");
-                Console.WriteLine("Press any key to continue...");
+                PressExit();
                 Console.ReadKey();
                 return;
             }
 
-            Defines.fileName = args[1];
+            Def.fileName = args[1];
 
-            if (!File.Exists(Defines.fileName))
+            if (!File.Exists(Def.fileName))
             {
-                Console.WriteLine("File {0} doesnt exist", Defines.fileName);
-                Console.WriteLine("Press any key to continue...");
+                Console.WriteLine("File {0} doesnt exist", Def.fileName);
+                PressExit();
+                Console.ReadKey();
                 return;
             }
 
-            //Lets take this to other thread
+            //Lets take this to other thread, this one will be waiting for program exit
             Thread mainThread = new Thread(new ThreadStart(StartMainThread));
             mainThread.Start();
 
-            while ((Console.ReadLine() != string.Format("exit") && Defines.programExit == 0) && Defines.programExit == 0)
+            while ((Console.ReadLine() != string.Format("exit") && Def.programExit == 0) && Def.programExit == 0)
                 continue;
 
             return;
@@ -59,23 +76,42 @@ namespace WowheadRipper
 
         static void StartMainThread()
         {
-            var lines = File.ReadAllLines(Defines.fileName);
+            var lines = File.ReadAllLines(Def.fileName);
             Int32 count = lines.Length;
 
-            StreamReader reader = new StreamReader(Defines.fileName);
-
             if (count != 0)
-                Console.WriteLine("Sucesfully loaded file {0}, {1} records found", Defines.fileName, count);
+                Console.WriteLine("Sucesfully loaded file {0}, {1} records found, please wait till parser starts parsing", Def.fileName, count);
             else
             {
-                Console.WriteLine("File {0} is empty", Defines.fileName);
-                Console.WriteLine("Press any key to continue...");
-                Defines.programExit = 1;
+                Console.WriteLine("File {0} is empty", Def.fileName);
+                PressExit();
+                Def.programExit = 1;
             }
 
-            datat = (UInt32)count;
             Console.WriteLine("");
 
+            StreamReader tester = new StreamReader(Def.fileName);
+            while (tester.Peek() >= 0)
+            {
+                string str = tester.ReadLine();
+                string[] numbers = Regex.Split(str, @"\D");
+
+                if (numbers.Length != 3)
+                    continue;
+
+                UInt32 typeId = UInt32.Parse(numbers[0]);
+                UInt32 subTypeIdFlag = UInt32.Parse(numbers[1]);
+    
+                if (typeId >= Def.GetMaxTypeId())
+                    continue;
+
+                if (subTypeIdFlag >= (1 << (int)Def.GetMaxTypeId()))
+                    continue;
+
+                datat += (UInt32)Def.GetValidFlags(typeId, subTypeIdFlag);
+            }
+
+            StreamReader reader = new StreamReader(Def.fileName);
             Thread writerThread = new Thread(new ThreadStart(WriterThread));
             writerThread.Start();
 
@@ -84,39 +120,46 @@ namespace WowheadRipper
                 string str = reader.ReadLine();
                 string[] numbers = Regex.Split(str, @"\D");
 
-                if (numbers.Length != 2)
+                if (numbers.Length != 3)
                 {
                     Console.WriteLine("Incorrect format for {0}, skipping", str);
                     continue;
                 }
 
-                UInt32 type =  UInt32.Parse(numbers[0]);
-                UInt32 entry =  UInt32.Parse(numbers[1]);
+                UInt32 typeId = UInt32.Parse(numbers[0]);
+                UInt32 subTypeIdFlag = UInt32.Parse(numbers[1]);
+                UInt32 entry = UInt32.Parse(numbers[2]);
 
-                if (type > Defines.maxType)
+                if (typeId >= Def.GetMaxTypeId())
                 {
-                    Console.WriteLine("Incorrect type {0} for {1}, skipping", type, entry);
+                    Console.WriteLine("Incorrect TypeId {0} for {1}, skipping", typeId, entry);
                     continue;
                 }
 
-                ThreadStart starter = delegate { ParseData(type, entry); };
+                if (subTypeIdFlag >= (1 << (int)Def.GetMaxTypeId()))
+                {
+                    Console.WriteLine("Incorrect SubTypeId Flag {0} for {1}, skipping", subTypeIdFlag, entry);
+                    continue;
+                }
+
+                ThreadStart starter = delegate { ParseData(typeId, subTypeIdFlag, entry); };
                 Thread thread = new Thread(starter);
                 thread.Start();
-
                 Thread.Sleep(700); // Else connections will time out or your net will go down
             }
             return;
         }
 
-        public static void ParseData(UInt32 type, UInt32 entry)
+        public static void ParseData(UInt32 typeId, UInt32 subTypeIdFlag, UInt32 entry)
         {
             UInt32 totalCount = 0;
             UInt32 count = 0;
             List<string> content;
+            List<int> ids = ExtractFlags(typeId, (Int32)subTypeIdFlag);
 
             try
             {
-                content = ReadPage(Defines.GenerateWowheadUrl(type, entry));
+                content = ReadPage(Def.GenerateWowheadUrl(typeId, entry));
             }
             catch (Exception e)
             {
@@ -125,79 +168,83 @@ namespace WowheadRipper
                 return;
             }
 
-            foreach (string line in content)
+            foreach (int tId in ids)
             {
-                Regex dataRegex = Defines.GetDataRegex(type);
-                Regex totalCountRegex = Defines.GetTotalCountRegex(type);
-
-                Match m = dataRegex.Match(line);
-                Match m2 = totalCountRegex.Match(line);
-
-                if (m2.Success)
+                UInt32 subTypeId = (UInt32)tId;
+                foreach (string line in content)
                 {
-                    string str = m2.Groups[0].Captures[0].Value;
-                    string[] numbers = Regex.Split(str, @"\D+");
-                    totalCount = uint.Parse(numbers[2]);
-                }
+                    Regex dataRegex = Def.GetDataRegex(typeId, subTypeId);
+                    Regex totalCountRegex = Def.GetTotalCountRegex(typeId, subTypeId);
 
-                if (!m.Success)
-                    continue;
+                    Match m = dataRegex.Match(line);
+                    Match m2 = totalCountRegex.Match(line);
 
-                var json = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
-                string data = m.Groups[1].Captures[0].Value;
-                data = data.Replace("[,", "[0,");   // otherwise deserializer complains
-                object[] m_object = (object[])json.DeserializeObject(data);
-
-                AddToStream(string.Format("-- Parsing {0} loot for entry {1}", Defines.id_name[type], entry));
-                AddToStream(string.Format("DELETE FROM `{0}` WHERE entry = {1};", Defines.db_name[type], entry));
-                AddToStream("");
-
-                foreach (dict objectInto in m_object)
-                {
-                    try
+                    if (m2.Success)
                     {
-                        count++;
-                        int id = (int)objectInto["id"];
-                        int maxcount = 1;
-                        int mincount = 1;
-                        float pct = 0.0f;
-                        string name = "";
-                        int lootmode = 0;
-
-                        if (type == 0)
-                            lootmode = 1;
-
-                        if (objectInto.ContainsKey("name"))
-                            name = (string)objectInto["name"];
-                        int m_count = (int)objectInto["count"];
-                        int ArraySize = ((Array)objectInto["stack"]).GetLength(0);
-                        int[] stack = new int[ArraySize];
-                        Array.Copy((Array)objectInto["stack"], stack, ArraySize);
-                        pct = (float)m_count / totalCount * 100.0f;
-                        maxcount = stack[1];
-                        mincount = stack[0];
-                        pct = (float)Math.Round(pct, 3);
-                        string strpct = pct.ToString();
-                        strpct = strpct.Replace(",", "."); // needs to be changed otherwise SQL errors
-                        string str = string.Format("INSERT INTO `{0}` VALUES ( '{1}', '{2}', '{3}', '{4}', '{5}', '{6}' , '{7}'); -- {8}",
-                        Defines.db_name[type], entry, id, strpct, 1, lootmode, mincount, maxcount, name);
-                        AddToStream(str);
+                        string str = m2.Groups[0].Captures[0].Value;
+                        string[] numbers = Regex.Split(str, @"\D+");
+                        totalCount = uint.Parse(numbers[2]);
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
 
-                if (count != 0)
-                {
-                    AddToStream(string.Format("-- Parsed {0} loot for entry {1}", Defines.id_name[type], entry));
+                    if (!m.Success)
+                        continue;
+
+                    var json = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
+                    string data = m.Groups[1].Captures[0].Value;
+                    data = data.Replace("[,", "[0,");   // otherwise deserializer complains
+                    object[] m_object = (object[])json.DeserializeObject(data);
+
+                    AddToStream(string.Format("-- Parsing {0} loot for entry {1}", Def.GetOutputName(typeId, subTypeId), entry));
+                    AddToStream(string.Format("DELETE FROM `{0}` WHERE entry = {1};", Def.GetDBName(typeId, subTypeId), entry));
                     AddToStream("");
-                }
 
-                Console.WriteLine("Parsed {0} loot for entry {1}", Defines.id_name[type], entry);
+                    foreach (dict objectInto in m_object)
+                    {
+                        try
+                        {
+                            count++;
+                            int id = (int)objectInto["id"];
+                            int maxcount = 1;
+                            int mincount = 1;
+                            float pct = 0.0f;
+                            string name = "";
+                            int lootmode = 0;
+
+                            if (typeId == 0 && subTypeId == 0)  // Only one loot in fishing
+                                lootmode = 1;
+
+                            if (objectInto.ContainsKey("name"))
+                                name = (string)objectInto["name"];
+                            int m_count = (int)objectInto["count"];
+                            int ArraySize = ((Array)objectInto["stack"]).GetLength(0);
+                            int[] stack = new int[ArraySize];
+                            Array.Copy((Array)objectInto["stack"], stack, ArraySize);
+                            pct = (float)m_count / totalCount * 100.0f;
+                            maxcount = stack[1];
+                            mincount = stack[0];
+                            pct = (float)Math.Round(pct, 3);
+                            string strpct = pct.ToString();
+                            strpct = strpct.Replace(",", "."); // needs to be changed otherwise SQL errors
+                            string str = string.Format("INSERT INTO `{0}` VALUES ( '{1}', '{2}', '{3}', '{4}', '{5}', '{6}' , '{7}'); -- {8}",
+                            Def.GetDBName(typeId, subTypeId), entry, id, strpct, 1, lootmode, mincount, maxcount, name);
+                            AddToStream(str);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+
+                    if (count != 0)
+                    {
+                        AddToStream(string.Format("-- Parsed {0} loot for entry {1}", Def.GetOutputName(typeId, subTypeId), entry));
+                        AddToStream("");
+                    }
+
+                    Console.WriteLine("Parsed {0} loot for entry {1}", Def.GetOutputName(typeId, subTypeId), entry);
+                }
+                datad++;
             }
-            datad++;
             return;
         }
 
@@ -206,34 +253,35 @@ namespace WowheadRipper
             StreamWriter file = new StreamWriter("parsed_data.sql", true);
             file.AutoFlush = true;
 
-            while (true && Defines.programExit == 0)
+            while (true && Def.programExit == 0)
             {
                 UInt32 dataWritten = 0;
-                Queue<string> copy = Defines.stream; // prevent crash
+                Queue<string> copy = Def.stream; // prevent crash
                 foreach (string str in copy)
                 {
                     dataWritten = 1;
                     file.WriteLine(str);
                 }
 
-                if (datad == datat && Defines.stream.Count == 0)
-                    Defines.programExit = 1;
+                if (datad == datat && Def.stream.Count == 0)
+                    Def.programExit = 1;
 
                 Thread.Sleep(100);
 
                 if (dataWritten == 1)
-                    Defines.stream.Clear();
+                    Def.stream.Clear();
             }
 
             file.Flush();
             file.Close();
             Console.WriteLine("");
             Console.WriteLine("Parsing done");
-            Console.WriteLine("Press any key to continue...");
+            PressExit();
             Console.Beep();
 
             return;
         }
+
         static List<string> ReadPage(string url)
         {
             HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
